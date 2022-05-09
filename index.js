764effa0375209a7ca68222a77383a9ae5516428
asrafaliii <asraf.bookaand@gmail.com>
2022-05-09 13:06:54 +0600

    connect to mongo db with password on env

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
 // use middleware
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wqazo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  console.log('fridgey db connected');
+  // perform actions on the collection object
+  client.close();
+});
+
 
 app.get('/', (req, res) =>{
     res.send('Hello World');
