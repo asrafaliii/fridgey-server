7997fea29cc29f3b69772307e026fc9a379e7bc5
asrafaliii <asraf.bookaand@gmail.com>
2022-05-11 19:32:51 +0600

    connect mongodb and insert data

--- index.js
+++ index.js
@@ -1,29 +1,47 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const { query } = require('express');
+
 const app = express();
 const port = process.env.PORT || 5000;
 
-// use middleware
 app.use(cors());
 app.use(express.json());
 
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wqazo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  console.log('fridgey db connected');
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run() {
+    try {
+        await client.connect();
+        const productsCollection = client.db('fridgey').collection('products');
+
+        app.get('/products', async (req, res) => {
+            const query = {};
+            const cursor = productsCollection.find(query);
+            const product = await cursor.toArray();
+            res.send(product);
+        });
 
 
-app.get('/', (req, res) =>{
+    }
+    finally {
+
+    }
+
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
-app.listen(port, () =>{
+app.listen(port, () => {
     console.log('server is running');
 })
